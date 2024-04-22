caf85403a3c73f13dacec61243cd4c590bc027ea
tw-seo <tw.seo@xiilab.com>
2024-04-22 15:44:48 +0900

    upload

--- python_train.sh
+++ python_train.sh
@@ -1,19 +1,1 @@
-# imgsz_list="640 1280"
-# batch_list="1 2 4 8 16"
-
-
-# for imgsz in $imgsz_list
-# do
-#     for batch in $batch_list
-#     do
-#         python train.py \
-#             --batch $batch \
-#             --epoch 100 \
-#             --imgsz $imgsz \
-#             --lr 0.001 \
-#             --pretrained True
-#     done
-# done
-
-
 python train.py --batch 16  --epoch 100 --imgsz 1280 --lr 0.001 --pretrained True
